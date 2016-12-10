f97a2dc1732ef62e0e503a427fca2dd748d4bbcb
Jason <jason.daurus@gmail.com>
2016-12-10 13:26:31 +0800

    print stdout & bug fix

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const cp = require('child_process')
 const path = require('path')
-const promisify = require('es6-promisify')
-
-const exec = promisify(cp.exec, { multiArgs: true })
+const chalk = require('chalk')
 
 module.exports = function x(cmds) {
   let promise = Promise.resolve()
 
-  for (const cmd of cmds.trim().split('\n')) {
-    promise.then(() => exec(cmd.trim()))
+  for (const line of cmds.replace(/\\\n/g, '').trim().split('\n')) {
+    const cmd = line.trim()
+
+    promise = promise.then(() => new Promise((resolve, reject) => {
+      process.stdout.write(`$ ${cmd}\n`)
+
+      const child = cp.exec(cmd)
+
+      child.stdout.pipe(process.stdout)
+      child.stderr.pipe(process.stderr)
+
+      child.once('close', (code, signal) => {
+        if (!code) {
+          return resolve()
+        }
+        reject(new Error(`${cmd} exit with code ${code}`))
+      })
+    }))
   }
 
   return promise
